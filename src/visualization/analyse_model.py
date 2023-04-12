5130419021411b12af3e8b12b11e504e57c29878
Michelle Lo <lomichelle42@gmail.com>
2023-04-12 21:03:29 +0800

    Add code for identifying concept neurons

---
+++ src/visualization/analyse_model.py
@@ -0,0 +1,86 @@
+import os
+import neurox.data.extraction.transformers_extractor as transformers_extractor
+import neurox.data.loader as data_loader
+import neurox.interpretation.utils as utils
+import neurox.interpretation.linear_probe as linear_probe
+import neurox.interpretation.clustering as clustering
+import neurox.interpretation.probeless as probeless
+import neurox.analysis.corpus as corpus
+from collections import defaultdict
+from src import token_inputs_path, token_labels_path, model_checkpoint
+
+
+class analyse_model:
+    def __init__(self, model_path, activations_path) -> None:
+        self.activations = None
+        self.tokens = None
+        self.X = None
+        self.y = None
+        self.idx2label = None
+        self.label2idx = None
+        self.probe = None
+        self.cluster_labels = None
+        self.cluster_map = defaultdict(list)
+
+        self.load_activations(model_path, activations_path)
+        self.load_tokens()
+        self.train_probe()
+
+    def load_activations(self, model_path, activations_path):
+        model_path_type = model_path + model_checkpoint
+        transformers_extractor.extract_representations(
+            model_path_type, token_inputs_path, activations_path, aggregation="average"
+        )
+        os.system("!cut -c1-120 < " + activations_path)
+        self.activations, num_layers = data_loader.load_activations(activations_path)
+
+    def load_tokens(self):
+        self.tokens = data_loader.load_data(
+            token_inputs_path, token_labels_path, self.activations, 512
+        )
+        self.X, self.y, mapping = utils.create_tensors(
+            self.tokens, self.activations, "NN"
+        )
+        self.label2idx, self.idx2label, src2idx, idx2src = mapping
+
+    def train_probe(self):
+        self.probe = linear_probe.train_logistic_regression_probe(self.X, self.y)
+        # Evaluate probe metrics
+        scores = linear_probe.evaluate_probe(
+            self.probe, self.X, self.y, idx_to_class=self.idx2label
+        )
+        print(scores)
+
+    def identify_clusters(self):
+        self.cluster_labels = clustering.create_correlation_clusters(self.X)
+        # Convert cluster labels list to dictionary
+        for idx in range(len(self.cluster_labels)):
+            label = self.cluster_labels[idx]
+            self.cluster_map[label].append(idx)
+
+    def identify_concept_neurons(self):
+        self.identify_clusters()
+        top_neurons = probeless.get_neuron_ordering_for_tag(
+            self.X, self.y, self.label2idx, "SEM:named_entity:location"
+        )
+
+        # Identify clusters containing concept neurons
+        concept_clusters = []
+        for neuron_idx in top_neurons:
+            if self.cluster_labels[neuron_idx] - 1 not in concept_clusters:
+                concept_clusters.append(self.cluster_labels[neuron_idx] - 1)
+
+        # get all neurons associated with identified clusters
+        associated_neurons = []
+        for cluster_idx in concept_clusters:
+            associated_neurons += self.cluster_map[cluster_idx]
+        print(associated_neurons)
+
+        return associated_neurons
+
+    def show_top_words(self, concept_neurons):
+        for neuron_idx in concept_neurons:
+            print(
+                neuron_idx,
+                corpus.get_top_words(self.tokens, self.activations, neuron_idx),
+            )
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,7 +2,14 @@ from models.train_model import train_model
 from features.build_features import load_token_class_dataset
 from models.evaluate_model import evaluate
 from models.prune_model import prune_model
-from src import basic_model_path, pruned_model_path, retrained_model_path
+from analyse_model import analyse_model
+from src import (
+    basic_model_path,
+    pruned_model_path,
+    retrained_model_path,
+    basic_activations_path,
+    retrained_activations_path,
+)
 
 
 def visualise():
@@ -12,8 +19,10 @@ def visualise():
     basic_model = model_trainer.train_basic_model()
     basic_model.save_pretrained(basic_model_path)
     evaluate(basic_model, dataset["validation"])
-    # TODO: Identify neurons in the basic neuron to ablate
-    neurons_to_prune = []
+    # Identify neurons in the basic neuron to ablate
+    basic_analyser = analyse_model(basic_model_path, basic_activations_path)
+    neurons_to_prune = basic_analyser.identify_concept_neurons()
+    basic_analyser.show_top_words(neurons_to_prune)
     # Ablate neurons
     pruned_model = prune_model(basic_model_path, model_trainer, neurons_to_prune)
     pruned_model.save_pretrained(pruned_model_path)
@@ -21,4 +30,8 @@ def visualise():
     # Retrain the pruned model
     retrained_model = model_trainer.retrain_pruned_model(pruned_model_path)
     retrained_model.save_pretrained(retrained_model_path)
-    # TODO: Examine the retrained model for concepts
+    # Examine the retrained model for concepts and compare with old model
+    retrained_analyser = analyse_model(retrained_model_path, retrained_activations_path)
+    new_concept_neurons = retrained_analyser.identify_concept_neurons()
+    retrained_analyser.show_top_words(new_concept_neurons)
+    basic_analyser.show_top_words(new_concept_neurons)
