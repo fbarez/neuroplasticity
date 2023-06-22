8d3fe78de3451a2706ddc7b3f827615a4a655f5d
Michelle Lo <lomichelle42@gmail.com>
2023-06-22 22:20:53 +0100

    Add notebooks

--- analyse_remap.py
+++ analyse_remap.py
@@ -1,46 +1,52 @@
-import gensim.downloader
+import gensim.downloader as api
+from gensim.models.fasttext import FastText
 import pandas as pd
 import ast
 
-PROCESSED_1 = "data/processed/retrained_name_model_1_concepts.csv"
-PROCESSED_2 = "data/processed/retrained_name_model_2_concepts.csv"
-PROCESSED_3 = "data/processed/retrained_name_model_3_concepts.csv"
-PROCESSED_4 = "data/processed/retrained_name_model_4_concepts.csv"
+from src import PROCESSED_1, PROCESSED_2, PROCESSED_3, PROCESSED_4
+from src import SIMILARITY_1, SIMILARITY_2, SIMILARITY_3, SIMILARITY_4
 
-def extract_pre_words(row, pre_words_col):
-    string_data = row[pre_words_col]
+def extract_pre_words(row):
+    string_data = row["current_concepts"]
     pre_words = [word for word, _ in ast.literal_eval(string_data)]
     return pre_words
 
-def extract_post_words(row, post_words_col):
-    string_data = row[post_words_col]
+def extract_post_words(row, col_name):
+    string_data = row[col_name]
     post_words = [word for word, _ in ast.literal_eval(string_data)]
     return post_words
 
-def vectorise(words, conceptnet):
-    """ Return set of word embeddings for given list of words. """
-    embeddings = []
-    for word in words:
-        if conceptnet.wv.has_index_for(word):
-            embeddings.append(model.wv[word])
-    return set(embeddings)
-
-def compare_similarity(dataset_path, pre_words_col, post_words_col, num):
-    conceptnet = gensim.downloader.load('conceptnet-numberbatch-17-06-300')
+def compare_similarity(dataset_path):
     df = pd.read_csv(dataset_path)
+    new_df = pd.DataFrame()
+    new_df["neuron-id"] = df["neuron-id"]
+    new_df["pre_words"] = df["current_concepts"]
+    new_df["post_words"] = df["concepts_1"]
     for index, row in df.iterrows():
-        if index > 5: break
-        pre_words = extract_pre_words(row, pre_words_col)
-        print(pre_words)
-        post_words = extract_post_words(row, post_words_col)
-        print(post_words)
-        df.at[index, "similarity"] = conceptnet.n_similarity(pre_words, post_words)
-    print(df)
-    df.to_csv(f'data/processed/similarity_{num}.csv')
-    return df
+        pre_words = extract_pre_words(row)
+        post_words = extract_post_words(row, "concepts_1")
+        new_df.at[index, "pre_words"] = pre_words
+        new_df.at[index, "post_words"] = post_words
+        new_df.at[index, "similarity"] = conceptnet.wv.n_similarity(pre_words, post_words)
+    return new_df
 
 if __name__ == '__main__':
-    compare_similarity(PROCESSED_1, "current_concepts", "concepts_1", 1)
-    # compare_similarity(PROCESSED_2, "current_concepts", "concepts_1", 2)
-    # compare_similarity(PROCESSED_3, "current_concepts", "concepts_1", 3)
-    # compare_similarity(PROCESSED_4, "current_concepts", "concepts_1", 4)
+
+    try:
+        conceptnet = FastText.load("fasttext.model")
+    except:
+        corpus = api.load('text8')
+        conceptnet = FastText(corpus)
+        conceptnet.save("fasttext.model")
+
+    retrained_1_df = compare_similarity(PROCESSED_1)
+    retrained_1_df.to_csv(SIMILARITY_1)
+
+    retrained_2_df = compare_similarity(PROCESSED_2)
+    retrained_2_df.to_csv(SIMILARITY_2)
+
+    retrained_3_df = compare_similarity(PROCESSED_3)
+    retrained_3_df.to_csv(SIMILARITY_3)
+
+    retrained_4_df = compare_similarity(PROCESSED_4)
+    retrained_4_df.to_csv(SIMILARITY_4)
