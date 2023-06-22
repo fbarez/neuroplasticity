081f6e6bb833673987a8a28f6fdeb5a049bf5ce3
Michelle Lo <michelle.lo21@imperial.ac.uk>
2023-06-22 10:27:22 +0100

    Modify settings for xlnet

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,7 @@
 from transformers import AutoTokenizer
 
 model_max_neurons = 768
-model_checkpoint = 'gpt2'
-# model_checkpoint = "distilbert-base-cased"
+model_checkpoint = 'xlnet-base-cased'
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 basic_model_path = "models/basic_model"
 pruned_model_path = "models/pruned_model"
